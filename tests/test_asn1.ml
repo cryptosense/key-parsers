00bf1d94d0166bf7d3e964d4507f9ab7a9935529
Nathan <nathan@cryptosense.com>
2016-03-08 11:12:35 +0100

    Add basic ASN1 DSA parsers tests

--- tests/test_asn1.ml
+++ tests/test_asn1.ml
@@ -81,6 +81,73 @@ let rsa_suite =
   ; "PKCS8" >::: pkcs8_suite
   ]
 
+let dsa_suite =
+  let open Key_parsers.DSA in
+  (* These parameters and key pair were generated using openssl dsaparam and gendsa *)
+  let expected_params =
+    let p =
+      Z.of_string
+        "0x00BD4C1FA40C9BE36677E2E4BB20892D67074CCA194D0D0103DE95700BA167E7931C\
+         BFEF3C1BA77E0BB18EBE7CC4393DF8A6C5A7C24FB9FCF993B7E75E77D594ADB51DDB00\
+         F9828858F8949B70090414B83C90CF085EC4C09DCB887BE7DBD623136AFCCA7600E7B6\
+         C332AC432DC673DA1FF7FF0A61B32D5C1B7BFE88B5BBC6DF15"
+    in
+    let q = Z.of_string "0x00B14CECC2CB93C59BC906964C4E44E9CEEADB87FD" in
+    let g =
+      Z.of_string
+        "0x7DDF10D1FB7215A714D83B332314AF0122996717094B6E0358F38CB4C16433C67CB0\
+         A18C2B687484AA53C9EB09A3E4E27F25EB4A42B8C578FC51058DEA8BDC9F417450BD84\
+         D15CCAC7111545E0EB9A50BCDF50E654CF579DEA5922CE6DC98C1FD3CCD26E68B30777\
+         E6101B0BAB004411152EB6D4064EB71677ACAB9943A7A3ED"
+    in
+    Params.{ p; q; g }
+  in
+  let expected_public =
+    let y =
+      Z.of_string
+        "0x7B37B6DBC2A1990C2FAB6A0E88FAF232F6BD4B4D0401F61A64D2AB842ECC0AE656A6\
+         0FB2D766F5B15F0EFABD7526B0D57B80A6AC8C8986EBB9A8303C6D651F6189A596E468\
+         51B724C15332280F6328E46ADF77D86A86E89CE5EF29E9413A3EBA4432657F6D4141AC\
+         E1CB3459AF366B2D96B4BA46A8042C2345F6B154E30CBFB6"
+    in
+    (expected_params, y)
+  in
+  let expected_private =
+    let x = Z.of_string "0x00A9CE6B152FE9832B504A2ECE260E29B7B812CDE8" in
+    (expected_params, x)
+  in
+  let cmp = Z.equal in
+  let printer = Z.to_string in
+  let test_params expected real ctxt =
+    let open Params in
+    assert_equal ~ctxt ~cmp ~printer ~msg:"p" expected.p real.p;
+    assert_equal ~ctxt ~cmp ~printer ~msg:"q" expected.q real.q;
+    assert_equal ~ctxt ~cmp ~printer ~msg:"g" expected.g real.g
+  in
+  let test ~typ ~decode (expected_params, expected_key) der ctxt =
+    let real = decode der in
+    let msg = match typ with `Public-> "y" | `Private -> "x" in
+    Test_util.assert_ok real @@ fun (real_params, real_key) ->
+    test_params expected_params real_params ctxt;
+    assert_equal ~ctxt ~cmp ~printer ~msg expected_key real_key
+  in
+  let x509_suite =
+    let typ = `Public in
+    let der = Cstruct.of_string @@ [%blob "../tests/keys/dsa_x509.der"] in
+    [ "Public" >:: test ~typ ~decode:Key_parsers.X509.decode_dsa expected_public der
+    ]
+  in
+  let pkcs8_suite =
+    let typ = `Private in
+    let der = Cstruct.of_string @@ [%blob "../tests/keys/dsa_pkcs8.der"] in
+    [ "Private" >:: test ~typ ~decode:Key_parsers.PKCS8.decode_dsa expected_private der
+    ]
+  in
+  [ "X509" >::: x509_suite
+  ; "PKCS8" >::: pkcs8_suite
+  ]
+
 let suite =
   [ "RSA" >::: rsa_suite
+  ; "DSA" >::: dsa_suite
   ]
