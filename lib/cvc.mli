c393e3037507280f9529f80e399c24ab3a7627e8
Bertrand Bonnefoy-Claudet <bertrand@cryptosense.com>
2020-11-27 17:08:23 +0100

    Remove dependency on ppx_bin_prot and ppx_deriving_yojson
    The values provided by those derivers were deprecated anyway.  The logic
    behind those deprecations was that it's not key-parsers' job to define
    and implement serialization formats for keys.  Since serialization isn't
    trivial (the format must be stable or there must be a migration path
    from one format to the next), it's better left to the user to define
    whatever they need for their application.

--- lib/cvc.mli
+++ lib/cvc.mli
@@ -10,26 +10,6 @@ sig
       }
     [@@deriving eq,ord,show]
 
-    val bin_t : t Bin_prot.Type_class.t0
-    [@@ocaml.deprecated "Bin_prot serializers will be removed in key-parsers 1.0.0"]
-    val bin_size_t : t -> int
-    [@@ocaml.deprecated "Bin_prot serializers will be removed in key-parsers 1.0.0"]
-    val bin_read_t : Bin_prot.Common.buf -> pos_ref:Bin_prot.Common.pos_ref -> t
-    [@@ocaml.deprecated "Bin_prot serializers will be removed in key-parsers 1.0.0"]
-    val bin_write_t : Bin_prot.Common.buf -> pos:int -> t -> int
-    [@@ocaml.deprecated "Bin_prot serializers will be removed in key-parsers 1.0.0"]
-    val bin_shape_t : Bin_prot.Shape.t
-    [@@ocaml.deprecated "Bin_prot serializers will be removed in key-parsers 1.0.0"]
-    val bin_reader_t : t Bin_prot.Type_class.reader0
-    [@@ocaml.deprecated "Bin_prot serializers will be removed in key-parsers 1.0.0"]
-    val bin_writer_t : t Bin_prot.Type_class.writer0
-    [@@ocaml.deprecated "Bin_prot serializers will be removed in key-parsers 1.0.0"]
-
-    val to_yojson : t -> Yojson.Safe.t
-    [@@ocaml.deprecated "Yojson serializers will be removed in key-parsers 1.0.0"]
-    val of_yojson : Yojson.Safe.t -> (t, string) result
-    [@@ocaml.deprecated "Yojson serializers will be removed in key-parsers 1.0.0"]
-
     val decode : Cstruct.t -> (t, string) Result.result
   end
 end
@@ -49,31 +29,6 @@ sig
       }
     [@@deriving eq,ord,show]
 
-    val bin_t : t Bin_prot.Type_class.t0
-    [@@ocaml.deprecated "Bin_prot serializers will be removed in key-parsers 1.0.0"]
-    val bin_size_t : t -> int
-    [@@ocaml.deprecated "Bin_prot serializers will be removed in key-parsers 1.0.0"]
-    val bin_read_t : Bin_prot.Common.buf -> pos_ref:Bin_prot.Common.pos_ref -> t
-    [@@ocaml.deprecated "Bin_prot serializers will be removed in key-parsers 1.0.0"]
-    val bin_write_t : Bin_prot.Common.buf -> pos:int -> t -> int
-    [@@ocaml.deprecated "Bin_prot serializers will be removed in key-parsers 1.0.0"]
-    val bin_shape_t : Bin_prot.Shape.t
-    [@@ocaml.deprecated "Bin_prot serializers will be removed in key-parsers 1.0.0"]
-    val bin_reader_t : t Bin_prot.Type_class.reader0
-    [@@ocaml.deprecated "Bin_prot serializers will be removed in key-parsers 1.0.0"]
-    val bin_writer_t : t Bin_prot.Type_class.writer0
-    [@@ocaml.deprecated "Bin_prot serializers will be removed in key-parsers 1.0.0"]
-
-    val to_yojson : t -> Yojson.Safe.t
-    [@@ocaml.deprecated "Yojson serializers will be removed in key-parsers 1.0.0"]
-    val of_yojson : Yojson.Safe.t -> (t, string) result
-    [@@ocaml.deprecated "Yojson serializers will be removed in key-parsers 1.0.0"]
-
     val decode : Cstruct.t -> (t, string) Result.result
   end
 end
-
-module RSA = Rsa
-[@@ocaml.deprecated "Use module Rsa instead"]
-module EC = Ec
-[@@ocaml.deprecated "Use module Ec instead"]
